5f1056ff4f28a0fefa76d65545fa94bc77bfddc8
ChuYaHsu <t199511137@gmail.com>
2022-12-31 01:29:15 +0800

    404  page

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const express = require('express');
 
 const app = express();
 
+
 //路由設定，routes
 app.get('/', (req,res)=>{
   res.send(`<h1>泥好</h1>`);
 
 });
+
+//*****所有路由設定都要放在這行之前*****
+app.use((req,res)=>{
+  res.type('text/html');
+  res.status(404).send(`<h1>404找不到你要的頁面</h1>`);
+}); 
+
+
+
+//*****所有路由設定都要放在這行之前*****
 const port = process.env.PORT ||3001;
 app.listen(port,()=>{
   console.log(`server started: ${port}`);
