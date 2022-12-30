4ea1a7f37a4dce286bea50271094f3279f520197
ChuYaHsu <t199511137@gmail.com>
2022-12-30 23:07:19 +0800

    讀取檔案2

--- src/http-server03.js
+++ src/http-server03.js
@@ -4,7 +4,10 @@ const fs = require('fs/promises');  //使用promise api
 
 const server = http.createServer(async(req, res)=>{
   console.log(`--------------`, req.url);
-  const result=  await fs.readFile(__dirname + '/esm01.html'
+  res.writeHead(200,{
+    'Content-Type':'text/plain'
+  });
+  const result=  await fs.readFile(__dirname + '/header01.txt'
   );
   console.log(result.toString());
   res.end(result.toString())
