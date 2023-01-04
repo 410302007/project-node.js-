20d3aa4a7fb872a2002e5c3690787c98bfededdc
ChuYaHsu <t199511137@gmail.com>
2023-01-04 23:41:44 +0800

    totalPages

--- routes/address-book.js
+++ routes/address-book.js
@@ -4,10 +4,15 @@ const db = require('../modules/connect-mysql');
 const router = express.Router();
 
 router.get('/', async(req, res)=>{
-  const t_sql = "SELECT COUNT(1) totalRows FROM member";
-  const [[{totalRows}]] = await db.query(t_sql);  //解構
+  let page = +req.query.page || 1; 
+  //用戶想要看第幾頁
+  //加號->轉換成數值
 
-  res.json(totalRows);
+  const perPage = 20;
+  const t_sql = "SELECT COUNT(1) num FROM member";  //總筆數
+  const [[{num : totalRows}]] = await db.query(t_sql);  //解構
+  const totalPages = Math.ceil(totalRows/perPage);  //總頁數
+  res.json({totalRows, totalPages, page});
 });
    
 module.exports = router;
