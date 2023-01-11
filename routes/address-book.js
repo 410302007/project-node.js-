ae81d85c4143f2dedf88924586e04b9dab3181f1
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-11 17:05:17 +0800

    刪除資料

--- routes/address-book.js
+++ routes/address-book.js
@@ -6,6 +6,16 @@ const moment = require('moment-timezone');
 
 const router = express.Router();
 
+//針對此模組的頂層 ; 經過路由前，會先經過此middleware
+//url, baseUrl, originalUrl 要在這裡拿，若在index那裡拿，originalUrl會一樣，但url & baseUrl會不同 
+router.use((req, res, next)=>{
+  const{url, baseUrl, originalUrl} = req;
+  res.locals={...res.locals,url, baseUrl, originalUrl};
+  //不能使用-> res.locals.url = url (會將先前在index設定的middleware排除)
+  next();
+});
+
+
 const getListData = async(req, res)=>{
   const output ={
     totalRows:0,
