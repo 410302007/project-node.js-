60682752d023ed4b40b3dba55a95c82d67ef01a1
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-09 00:10:48 +0800

    加到最愛的API

--- routes/products.js
+++ routes/products.js
@@ -15,9 +15,45 @@ router.use((req, res, next)=>{
   next();
 });
 
+router.get('/toggle-like/:pid', async(req, res)=>{
+  const output={
+    success:false,
+    error:'',
+    action:'',
+  };
+  //必須為已登入的會員
+  if(! req.session.user){
+    output.error = '必須登入會員，才能將商品加到最愛!'
+    return res.json(output);
+  }
+
+  const sql1 = "SELECT * FROM product_likes WHERE `mid`=? AND `product_id`=?"
+  const [likes] = await db.query(sql1,[
+    req.session.user.id,
+    req.params.pid
+  ]);
+  if(likes.length){ //如果有收藏就取消
+    const sql2 = "DELETE FROM `product_likes` WHERE sid" + likes[0].sid;
+    const [result] = await db.query(sql2);
+    output.success = !! result.affectedRows;
+    output.action = 'delete';
+  }else{
+    //TODO: 判斷有無此商品
+
+    const sql3 = "INSERT INTO `product_likes`(`mid`, `product_id`) VALUES (?,?)";
+    const [result] = await db.query(sql3,[
+      req.session.user.id,
+      req.params.pid
+    ]);
+    output.success = !! result.affectedRows
+    output.action = 'insert';
+  }
+  res.json(output);
+});
+//products
 router.get('/', async(req, res)=>{
   const [rows] = await db.query("SELECT * FROM products");
-  res.send(rows);
+  res.json(rows);
 });
 
 
